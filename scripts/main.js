8660de3580fe9e50adbdd2a7f7d2ca0afa12fee6
beliga44 <beliga44@gmail.com>
2019-09-07 13:57:25 +0700

    fix view revision

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 (function(){
 	const productTitles = document.querySelectorAll('.type-container > .item');
+	const productCategories = document.querySelectorAll('.list-categories > .item');
 	let prevTargetId = 'indoor-camera';
 	productTitles.forEach((title) => {
 		title.onclick = function(e){
@@ -8,6 +9,15 @@
 			const targetId = prevTargetId = e.currentTarget.getAttribute('data-target');
 			const element = document.getElementById(targetId);
 			element.setAttribute('class', 'item d-flex bounce-inUp-1-6');
-		}	
+		}
 	});		
+	productCategories.forEach(category => {
+		category.onclick = (e) => {
+			const prevElement = document.getElementById(prevTargetId);
+			if(prevElement) prevElement.setAttribute('class', 'item hide');
+			const targetId = prevTargetId = e.currentTarget.getAttribute('data-target');
+			const element = document.getElementById(targetId);
+			element.setAttribute('class', 'item d-flex bounce-inUp-1-6');
+		}
+	});
 }());
